732ef9d3ea75ff42fb06d48eb17b945facc51bec
q400400200-pixel <q.400400200@gmail.com>
2025-10-21 22:35:36 +0300

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,39 +1,39 @@
-#include "binary_trees.h"
+ #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
- * @tree: Pointer to the root node of the tree to measure the height
- *
- * Return: Height of the tree, or 0 if tree is NULL
+ * height - measures the height of a binary tree (by nodes)
+ * @tree: pointer to the root node
+ * Return: height or 0 if tree is NULL
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+static size_t height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+    size_t left_h, right_h;
 
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return (0);
 
-	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+    left_h = height(tree->left);
+    right_h = height(tree->right);
 
-	return (left_height > right_height ? left_height : right_height);
+    if (left_h >= right_h)
+        return (left_h + 1);
+    else
+        return (right_h + 1);
 }
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree to measure the balance factor
- *
- * Return: Balance factor, or 0 if tree is NULL
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node
+ * Return: balance factor, or 0 if tree is NULL
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+    int left_h, right_h;
 
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return (0);
 
-	left_height = (int)binary_tree_height(tree->left);
-	right_height = (int)binary_tree_height(tree->right);
+    left_h = (int)height(tree->left);
+    right_h = (int)height(tree->right);
 
-	return (left_height - right_height);
-}
+    return (left_h - right_h);
